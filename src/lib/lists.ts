3dab280b58cd053759d3699e95cbbbb41ee3a970
Paula <paulafffl@gmail.com>
2024-03-14 11:17:56 +0000

    Populate all lists

--- src/lib/lists.ts
+++ src/lib/lists.ts
@@ -1,4 +1,103 @@
-export const listDefault = [
+export const listToiletries = [
+  '🧴 Alcohol gel',
+  '🌸 Deodorant',
+  '🦷 Toothpaste',
+  '🪥 Toothbrush',
+  '🧵 Floss',
+  '🧼 Body soap',
+  '🌝 Facial soap',
+  '💆‍♀️ Shampoo',
+  '🪮 Hair comb',
+  '🌞 Facial SPF',
+  '💋 Lip balm SPF',
+  '💦 Moisturizer',
+  '💊 Vitamins / Pills',
+  '🩹 Eco Bandage',
+  '💅 Nail file',
+  '🩸 Period Cup / Pants',
+];
+
+export const listZeroWaste = [
+  '🍴 Cutlery',
+  '🍱 Bento box',
+  '🥤 Straw',
+  '☕ Thermal Cup',
+  '🧽 Sponge',
+  '🧼 Dish Soap',
+  '🔶 Cloth',
+  '🍵 Mug',
+  '😷 Facemask',
+  '🧂 Seasonings',
+];
+
+export const listClothes = [
+  '👕 Shirts',
+  '👚 Tops',
+  '👖 Trousers',
+  '🩳 Shorts / Skirt',
+  '👗 Jumpsuit / Dress',
+  '🧥 Coat / Jacket',
+  '🩲 Underwear',
+  '🧦 Socks',
+  '🧘 Sportswear',
+  '😴 Pyjamas',
+];
+
+export const listFood = [
+  '🍫 Protein bar',
+  '🍩 Sweets',
+  '🍿 Savouries',
+  '🥐 Breakfast',
+  '🍎 Fruits',
+  '🥪 Sandwich',
+];
+
+export const listSummer = [
+  '🧴 Sun Protector',
+  '🏖️ Beach Towel',
+  '👙 Swimwear',
+  '🕶️ Sun Glasses',
+  '🩴 Flipflops',
+  '🧢 Hat',
+];
+
+export const listWinter = ['🧤 Gloves', '🪖 Beanie', '🧣 Scarf', '🧥 Sweater', '🧦 Warm socks'];
+
+export const listTech = [
+  '📱 Phone + charger',
+  '🔋 Power bank + charger',
+  '💻 Laptop + charger',
+  '🔊 Speaker + charger',
+  '🔌 Plug Adapter',
+  '📷 Camera',
+  '🔥 Sex toys + charger',
+  '💨 Vape + charger',
+  '🎧 Earplug splitter',
+];
+
+export const listEssentials = [
+  '📇 ID',
+  '🛂 Passport',
+  '💳 Credit card',
+  '🔑 Keys',
+  '🥽 Eye Mask',
+  '🎧 Earphones',
+  '👂 Earplugs',
+  '🍶 Water bottle',
+  '🧥 Raincoat',
+  '☔ Umbrella',
+  '👝 Fanny pack',
+  '✏️ Pencil',
+  '📓 Notebook',
+  '👟 Sneaker',
+  '🥾 Boots',
+  '🚊 Train Tickets',
+  '🚢 Boat Tickets',
+  '🛩 Airplane Tickets',
+  '🚍 Bus Tickets',
+];
+
+export const listCamping = [
   '⛺ Tent',
   '🛌 Sleeping Bag',
   '➖ Insulating Mat',
@@ -18,21 +117,14 @@ export const listDefault = [
   '✨ Bio Glitter',
   '🎟️ Festival Ticket',
   '💵 Cash',
-  '🍴 Cutlery',
-  '🍱 Bento box',
-  '🥤 Straw',
-  '🧽 Sponge',
-  '🧼 Cloth',
-  '🍵 Mug',
-  '😷 Facemask',
-  '🧂 Seasonings',
 ];
 
-export const listSummer = [
-  '🧴 Sun Protector',
-  '🏖️ Beach Towel',
-  '👙 Swimwear',
-  '🕶️ Sun Glasses',
-  '🩴 Flipflops',
-  '🧢 Hat',
+export const readyToGo = [
+  '🪴 Water plants',
+  '🗑️ Take out trash',
+  '🧊 Get chilled food',
+  '🔥 Turn off heating',
+  '💡 Turn off lights',
+  '📝 Review checklist',
+  '🚪 Look door',
 ];
