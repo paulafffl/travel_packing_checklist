756032398cec47a98bb86b2efe15f791d7b2a5c4
Paula <paulafffl@gmail.com>
2024-03-28 20:58:19 +0000

    Test all App elements

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -4,12 +4,40 @@ import App from './App';
 import { ItemsContext } from '../context';
 import { mockContextValue } from '../setupTests';
 
-test('renders page title', () => {
-  render(
-    <ItemsContext.Provider value={mockContextValue}>
-      <App />
-    </ItemsContext.Provider>,
-  );
-  const headerElement = screen.getByText(/travel packing/i);
-  expect(headerElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders header', () => {
+    render(
+      <ItemsContext.Provider value={mockContextValue}>
+        <App />
+      </ItemsContext.Provider>,
+    );
+    const headerElement = screen.getByText(/travel packing/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  test('renders main', () => {
+    render(
+      <ItemsContext.Provider value={mockContextValue}>
+        <App />
+      </ItemsContext.Provider>,
+    );
+    const sectionAddItems = screen.getByText(/^add items$/i);
+    expect(sectionAddItems).toBeInTheDocument();
+
+    const sectionPackedItems = screen.getByText(/^packed items$/i);
+    expect(sectionPackedItems).toBeInTheDocument();
+
+    const sectionUnpackedItems = screen.getByText(/^unpacked items$/i);
+    expect(sectionUnpackedItems).toBeInTheDocument();
+  });
+
+  test('renders footer', () => {
+    render(
+      <ItemsContext.Provider value={mockContextValue}>
+        <App />
+      </ItemsContext.Provider>,
+    );
+    const footerCredit = screen.getByText(/^paula lima$/i);
+    expect(footerCredit).toBeInTheDocument();
+  });
 });
