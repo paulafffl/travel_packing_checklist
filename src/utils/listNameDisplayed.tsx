2e110b6e0d3eed432b79b2bade3ae298b8956274
Paula <paulafffl@gmail.com>
2024-03-29 11:36:41 +0000

    Create listNameDisplay and listNameEmoji utils

---
+++ src/utils/listNameDisplayed.tsx
@@ -0,0 +1,13 @@
+import { listNameEmoji } from './listNameEmoji';
+
+export const listNameDisplay = (name: string) => {
+  const listName = name?.substring(4).toUpperCase();
+  return (
+    <>
+      <span role="img" aria-label={name} className={'m-0 p-0'}>
+        {listNameEmoji[name]}
+      </span>
+      &nbsp;<span className="font-bold">{listName}</span>
+    </>
+  );
+};
---
+++ src/utils/listNameEmoji.tsx
@@ -0,0 +1,17 @@
+type listNameEmoji = {
+  [key: string]: string;
+};
+
+export const listNameEmoji: listNameEmoji = {
+  listZeroWaste: '💚',
+  listFood: '🍎',
+  listTech: '⚡',
+  listEssentials: '🎒',
+  listClothes: '👕',
+  listLeaving: '🚪',
+  listSummer: '☀️',
+  listWinter: '❄️',
+  listToiletries: '🛁',
+  listCamping: '🏕️',
+  listAdditionals: '📝',
+};
