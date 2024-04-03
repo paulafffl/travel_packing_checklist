8d71670bf57861e6bba3ea84f1decd04fc9843e5
Paula <paulafffl@gmail.com>
2024-04-03 09:27:13 +0100

    Refactor displaySectionName

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -21,63 +21,60 @@ const List = ({ list, packed }: { list: string; packed: boolean }) => {
   const itemsShown = listsWithItemsShown.includes(list);
   const packedItems = listsObj[list]?.filter((item) => item.packed);
   const unpackedItems = listsObj[list]?.filter((item) => !item.packed);
+  const listShown = (packed && packedItems?.length > 0) || (!packed && unpackedItems?.length > 0);
 
-  const displaySectionName = (listName: string) => {
-    if ((packed && packedItems?.length > 0) || (!packed && unpackedItems?.length > 0)) {
-      return (
-        <div className="mb-1 mt-2 flex items-center justify-between border-b-2 border-slate-300 pb-2">
-          <div className="flex items-center">
-            <button
-              className={`m-0 mr-1 h-5 w-5 p-0.5 px-0.5 sm:mb-0.5 ${
-                itemsShown && 'color-palette-green'
-              }`}
-              aria-label={`Add List for ${listName}`}
-              title={itemsShown ? 'Hide items' : 'Show items'}
-              onClick={() => (itemsShown ? hideListItems(listName) : showListItems(listName))}
-            >
-              <Icon symbol={itemsShown ? 'collapse' : 'expand'} />
-            </button>
-            <span
-              className="m-0.5 cursor-pointer text-slate-400 sm:m-1"
-              onClick={() => (itemsShown ? hideListItems(listName) : showListItems(listName))}
-            >
-              {listNameDisplay(listName)}
-            </span>
-          </div>
-          <div>
-            <button
-              className={`ml-2 bg-white px-0.5 ${
-                listName === 'listAdditionals' ? 'disabled:bg-white' : 'hover:bg-indigo-200'
-              }`}
-              aria-label={`Reset "${listName}"`}
-              title={'Reset list'}
-              disabled={listName === 'listAdditionals'}
-              onClick={() => setModalResetList(listName)}
-            >
-              <Icon
-                symbol="reset"
-                color={listName === 'listAdditionals' ? 'lightgrey' : 'cornflowerblue'}
-              />
-            </button>
-            <button
-              className={`ml-2 bg-white px-0.5 ${
-                !!modalDeleteAdditionals ? 'disabled:bg-white' : 'hover:bg-rose-200'
-              }`}
-              aria-label={`Close "${listName}"`}
-              title={'Close list'}
-              disabled={!!modalDeleteAdditionals}
-              onClick={() =>
-                listName === 'listAdditionals'
-                  ? setModalDeleteAdditionals(true)
-                  : hideList(listName)
-              }
-            >
-              <Icon symbol="close" color={!!modalDeleteAdditionals ? 'grey' : 'red'} />
-            </button>
-          </div>
+  const listHeading = (listName: string) => {
+    return (
+      <div className="mb-1 mt-2 flex items-center justify-between border-b-2 border-slate-300 pb-2">
+        <div className="flex items-center">
+          <button
+            className={`m-0 mr-1 h-5 w-5 p-0.5 px-0.5 sm:mb-0.5 ${
+              itemsShown && 'color-palette-green'
+            }`}
+            aria-label={`Add List for ${listName}`}
+            title={itemsShown ? 'Hide items' : 'Show items'}
+            onClick={() => (itemsShown ? hideListItems(listName) : showListItems(listName))}
+          >
+            <Icon symbol={itemsShown ? 'collapse' : 'expand'} />
+          </button>
+          <span
+            className="m-0.5 cursor-pointer text-slate-400 sm:m-1"
+            onClick={() => (itemsShown ? hideListItems(listName) : showListItems(listName))}
+          >
+            {listNameDisplay(listName)}
+          </span>
         </div>
-      );
-    }
+        <div>
+          <button
+            className={`ml-2 bg-white px-0.5 ${
+              listName === 'listAdditionals' ? 'disabled:bg-white' : 'hover:bg-indigo-200'
+            }`}
+            aria-label={`Reset "${listName}"`}
+            title={'Reset list'}
+            disabled={listName === 'listAdditionals'}
+            onClick={() => setModalResetList(listName)}
+          >
+            <Icon
+              symbol="reset"
+              color={listName === 'listAdditionals' ? 'lightgrey' : 'cornflowerblue'}
+            />
+          </button>
+          <button
+            className={`ml-2 bg-white px-0.5 ${
+              !!modalDeleteAdditionals ? 'disabled:bg-white' : 'hover:bg-rose-200'
+            }`}
+            aria-label={`Close "${listName}"`}
+            title={'Close list'}
+            disabled={!!modalDeleteAdditionals}
+            onClick={() =>
+              listName === 'listAdditionals' ? setModalDeleteAdditionals(true) : hideList(listName)
+            }
+          >
+            <Icon symbol="close" color={!!modalDeleteAdditionals ? 'grey' : 'red'} />
+          </button>
+        </div>
+      </div>
+    );
   };
 
   const confirmAdditionalsDeletion = () => {
@@ -120,8 +117,8 @@ const List = ({ list, packed }: { list: string; packed: boolean }) => {
     <div>
       {modalDeleteAdditionals && confirmAdditionalsDeletion()}
       {modalResetList && confirmListReset()}
-      {displaySectionName(list)}
-      {itemsShown && displaySectionName(list) && (
+      {listShown && listHeading(list)}
+      {listShown && itemsShown && (
         <ul className="flex flex-col">
           {packed
             ? packedItems?.map((item) => <Item key={item.id} item={item} listName={list} />)
