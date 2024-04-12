d8e46de4452bda93f8d807ddbb982fa9d5b58f20
Paula <paulafffl@gmail.com>
2024-04-12 14:03:26 +0100

    Create textShadow to improve accessiblity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -8,10 +9,26 @@ module.exports = {
     extend: {
       colors: { ...colors, primary: colors.violet },
       spacing: { 0.2: '0.06rem' },
+      textShadow: {
+        sm: '1px 1px 0px var(--tw-shadow-color)',
+        DEFAULT: '2px 2px 0px var(--tw-shadow-color)',
+        lg: '3px 3px 3px var(--tw-shadow-color)',
+      },
       boxShadow: {
         '3xl': '0 7px 20px rgba(0, 0, 0, 0.3)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') },
+      );
+    }),
+  ],
 };
