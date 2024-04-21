433f042852ad0235d276d8aba8ed625189f64733
Paula <paulafffl@gmail.com>
2024-04-21 18:15:00 +0100

    Refinements for Safari and Firefox

--- src/utils/listNameDisplayed.tsx
+++ src/utils/listNameDisplayed.tsx
@@ -4,7 +4,7 @@ export const listNameDisplay = (name: string) => {
   const listName = name?.substring(4).toUpperCase();
   return (
     <>
-      <span role="img" aria-label={name} className={'m-0 p-0'}>
+      <span role="img" aria-label={name} className={'m-0 mr-0.2 p-0'}>
         {listNameEmoji[name]}
       </span>
       &nbsp;<span className="font-bold">{listName}</span>
